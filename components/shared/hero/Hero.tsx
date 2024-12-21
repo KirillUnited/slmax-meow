ccd632882e2799301359307706d692128af9db91
dubov <Kirill.Dubov@enterra-inc.com>
2024-12-21 18:09:41 +0300

    feat(Hero): Add text animations

--- components/shared/hero/Hero.tsx
+++ components/shared/hero/Hero.tsx
@@ -1,19 +1,26 @@
+'use client';
 import { title, subtitle } from "@/components/primitives";
-import Products from "@/components/shared/product/Products";
+import clsx from "clsx";
+import { motion } from "framer-motion";
+
+import styles from "./styles.module.css";
 
 export default function Hero() {
   return (
     <>
-      <section className="section flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-        <div className="container inline-block max-w-xl text-center justify-center">
-          <span className={title()}>Make&nbsp;</span>
-          <span className={title({ color: "violet" })}>your&nbsp;</span>
-          <br />
-          <span className={title()}>eCommerce.</span>
-          <div className={subtitle({ class: "mt-4" })}>
+      <section className={clsx("section", styles["hero-section"])}>
+        <motion.div animate={{ opacity: 1, transform: "translateY(0)" }} initial={{ opacity: 0, transform: "translateY(120px)" }} transition={{ duration: .7 }} className="container inline-block max-w-xl text-center justify-center">
+          <h1>
+            <span className={title()}>Make&nbsp;</span>
+            <span className={title({ color: "violet" })}>your&nbsp;</span>
+            <br />
+            <span className={title()}>eCommerce.</span>
+          </h1>
+          <motion.p initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }} transition={{ duration: 1.5 }} className={subtitle({ class: "mt-4" })}>
             Everything you need to have a great shop.
-          </div>
-        </div>
+          </motion.p>
+        </motion.div>
       </section>
     </>
   );
